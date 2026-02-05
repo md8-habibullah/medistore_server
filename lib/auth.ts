bd290f412424210619e982b3607138391a54ec35
HABIBULLAH <contact@habibullah.dev>
2026-02-05 15:08:20 +0600

    email connected for verify

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { send } from "node:process";
+import { sendVerificationEmail } from "./email";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -23,6 +25,17 @@ export const auth = betterAuth({
         autoSignIn: false,
         minPasswordLength: 1,
         requireEmailVerification: true,
+    },
+
+    emailVerification: {
+        sendOnSignUp: true,
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            sendVerificationEmail(user.email, url, token);
+            // console.log(`Send verification email to ${user.email} with url: ${url} and token: ${token}`);
+            // to: user.email,
+            // subject: "Verify your email address",
+            // text: `Click the link to verify your email: ${url}`,
+        },
     },
 
 });
