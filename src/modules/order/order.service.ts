6c8c1aee83bb67ae2e164c7afa3ea70c3da85aa7
HABIBULLAH <contact@habibullah.dev>
2026-03-09 00:46:04 +0600

    finally its working by tsup; thanks

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../../../lib/prisma.js";
+import { prisma } from "../../../lib/prisma";
 // import { Status } from "../../../generated/prisma/client"; // Import Status Enum
 
 // 1. Create Order (Customer)
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../lib/prisma.js"
+import { prisma } from "../lib/prisma"
 import app from "./app";
 
 const port = process.env.PORT ?? 5050;
